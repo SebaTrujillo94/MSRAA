1adcf1f90ddabb8a8509a678a398baf6aa67240c
SebaTrujillo94 <s.trijillo@duocuc.cl>
2026-07-09 21:34:28 -0400

    fix: add /admin/mantenedor/ shortcut redirect

--- msraa/urls.py
+++ msraa/urls.py
@@ -3,8 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core import views
+from django.urls import reverse
+from django.shortcuts import redirect
+
+def _mantenedor_redirect(request):
+    return redirect('/admin/core/siteconfiguration/mantenedor/')
 
 urlpatterns = [
+    path('admin/mantenedor/', admin.site.admin_view(_mantenedor_redirect), name='mantenedor_shortcut'),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('', views.index, name='index'),
